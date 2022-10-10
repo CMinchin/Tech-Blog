8ca2defc5fb1f3596a4d7b23ce0a98e5d97bc901
Equinox <cameron.minchin@gmail.com>
2022-10-10 11:06:18 +0800

    heroku stuff 8

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+console.log(process.env)
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
